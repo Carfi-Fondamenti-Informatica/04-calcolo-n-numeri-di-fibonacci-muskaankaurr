eb3a567a2c9377970a3d5984596b9d3df05fc63d
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-11-24 23:24:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int n=0, a=1; b=1 ; c=0;
+   int n=0, a=1, b=1 , c=0;
    cin >> n;
    if (n>=2) {
-      cout << a << endl << b << endl;
-     for (int i=2; i<n ; i++){
+      cout << a << b endl;
+     for (int i=2; i<n ; i++) {
         c=a+b;
         a=b;
         b=c;
         cout << c << endl;
+      }  
    }  
-     }  
   else { 
       cout << "errore" << endl;
        }   
